176f3fc48b38b1ed72c42195e007c7dcb352d2a7
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-09-23 22:46:02 +0600

    modified::project data

--- public/projectData/projectData.js
+++ public/projectData/projectData.js
@@ -1,8 +1,8 @@
 import kutirGhor from "/src/assets/kutir-ghor.jpg"
 import freshFood from "/src/assets/FreshFood.jpg"
 import travel from "/src/assets/Travel-community.jpg"
-import littleJoyFull from "/src/assets/Little-JoyFull/little-joyfull-1.jpg"
-import footballDen from "/src/assets/Football-Den/Football-Den-1.jpg"
+import littleJoyFull from "/src/assets/little-joyfull.jpg"
+import footballDen from "/src/assets/football-den.jpg"
 const projectData = [
   
   {
@@ -22,30 +22,18 @@ const projectData = [
   },
   {
     id: 2,
-    thumbnail: freshFood,
-    title: "Fresh Food",
+    thumbnail: footballDen,
+    title: "Football coach Den",
     description:
-      "Explore the taste of Bangladesh through a React-based website. Discover a wide variety of recipes from talented Bangladeshi chefs. With ExpressJS handling the server side and the convenience of Firebase authentication.",
-    liveLink: "https://favourite-food-recipes.web.app/",
+      "This website is based on kids. Here they can do multiple courses during their summer vacation. Built with ReactJS, ExpressJS, and MongoDB, it provides a seamless user experience and integrates features like Firebase and React Stripe for secure payments.",
+    liveLink: "https://the-football-coach-den.web.app/",
     github:[
-      {id:1,title:"client",link: "https://github.com/pm-rahman/frash-food-recipe-client"},
-      {id:2,title:"server",link: "https://github.com/pm-rahman/frash-food-recipe-server"}
+      {id:1,title:"client",link: "https://github.com/pm-rahman/Football-coatch-den-client"},
+      {id:2,title:"server",link: "https://github.com/pm-rahman/Football-Coach-Den-Server"}
     ]
   },
   {
     id: 3,
-    thumbnail: travel,
-    title: "Travel Community",
-    description:
-      "This is a Travel system application. There is a feature which will try to make a community among all travelers who will use the application. Any traveler can create his/her own community, where they can manage members from Dashboard. ",
-    liveLink: "https://travel-community-dd955.web.app/",
-    github:[
-      {id:1,title:"client",link: "https://github.com/pm-rahman/Travel-community-client"},
-      {id:2,title:"server",link: "https://github.com/pm-rahman/Travel-community-server"}
-    ]
-  },
-  {
-    id: 4,
     thumbnail: littleJoyFull,
     title: "Little Joyful Land",
     description:
@@ -57,15 +45,27 @@ const projectData = [
     ]
   },
   {
-    id: 5,
-    thumbnail: footballDen,
-    title: "Football coach Den",
+    id: 4,
+    thumbnail: freshFood,
+    title: "Fresh Food",
     description:
-      "This website is based on kids. Here they can do multiple courses during their summer vacation. Built with ReactJS, ExpressJS, and MongoDB, it provides a seamless user experience and integrates features like Firebase and React Stripe for secure payments.",
-    liveLink: "https://the-football-coach-den.web.app/",
+      "Explore the taste of Bangladesh through a React-based website. Discover a wide variety of recipes from talented Bangladeshi chefs. With ExpressJS handling the server side and the convenience of Firebase authentication.",
+    liveLink: "https://favourite-food-recipes.web.app/",
     github:[
-      {id:1,title:"client",link: "https://github.com/pm-rahman/Football-coatch-den-client"},
-      {id:2,title:"server",link: "https://github.com/pm-rahman/Football-Coach-Den-Server"}
+      {id:1,title:"client",link: "https://github.com/pm-rahman/frash-food-recipe-client"},
+      {id:2,title:"server",link: "https://github.com/pm-rahman/frash-food-recipe-server"}
+    ]
+  },
+  {
+    id: 5,
+    thumbnail: travel,
+    title: "Travel Community",
+    description:
+      "This is a Travel system application. There is a feature which will try to make a community among all travelers who will use the application. Any traveler can create his/her own community, where they can manage members from Dashboard. ",
+    liveLink: "https://travel-community-dd955.web.app/",
+    github:[
+      {id:1,title:"client",link: "https://github.com/pm-rahman/Travel-community-client"},
+      {id:2,title:"server",link: "https://github.com/pm-rahman/Travel-community-server"}
     ]
   },
 ];
